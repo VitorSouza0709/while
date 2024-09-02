af34ff89f27b9246b1b15ad17a8b91fe61b8b734
Vitor <169068391+VitorSouza0709@users.noreply.github.com>
2024-09-02 12:08:41 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Bom dia ");
+    int contador = 1;
+
+    while(contador < 11) {
+      System.out.println("Bom dia");
+      contador = contador + 1;
+      
+    }
+  }
+
+  
+}
